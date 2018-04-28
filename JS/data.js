1169753832af82b318b1b7bb39580e61ef29a57a
Jordan Diaz <JordanDiaz@Jordans-MacBook-Pro.local>
2018-04-28 11:55:12 -0400

    added javascript

---
+++ JS/data.js
@@ -0,0 +1,31 @@
+//tables data
+var tables = [
+    {
+        name: "John Smith", 
+        phoneNumber: "40712345678",
+        email: "john.smith@email.com", 
+        userID: "js123"
+
+        
+    },
+    {   
+        name:"Jane Smith", 
+        phoneNumber: "3124567890", 
+        email: "john.smith@email.com",
+        userID: "js456"
+    
+    }
+];
+
+//waitlist data
+var waitlist = [
+    {   
+                name:"Jane Smith", 
+                phoneNumber: "3124567890", 
+                email: "john.smith@email.com",
+                userID: "js456"
+            
+            }
+];
+
+module.exports = {tables, waitlist};
---
+++ JS/server.js
@@ -0,0 +1,43 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var dataFile = require("./data.js");
+
+//express app 
+var app = express();
+var PORT = 3000; 
+
+//data parsing 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//routes
+app.get("/api/tables", function(req, res) {
+    return res.json(dataFile.tables);
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(dataFile.waitlist);
+});
+
+
+app.post("/api/tables", function(req, res) {
+    if(dataFile.tables.length < 5 ){
+        dataFile.tables.push(req.body);
+        res.json(true);
+    }
+
+    // Or false if they don't have a table
+    else{
+        dataFile.waitlist.push(req.body);
+        res.json(false); // KEY LINE
+    }
+});
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
